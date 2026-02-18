ca4011bf85f8fc661986cfc7e09cde4681775a28
Rajat Agrawal <rajat@eplumbers.de>
2026-02-18 21:32:06 +0530

    adding origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,9 +12,11 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="HRMS Lite API")
 
+origins = os.environ.get("origins", "http://localhost:5173").split(",")
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # change in production
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
